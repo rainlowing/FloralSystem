0815c1a544579ac4d061b67cecd7e6b50e145c19
rainlowing <1620326574@qq.com>
2024-11-08 23:05:19 +0800

    从仓库中删去了不必要的文件（.vscode/ 和 build/）

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_FloralSystem_FILE D:/QtProjects/FloralSystem/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/FloralSystem.exe)
-set(__QT_DEPLOY_TARGET_FloralSystem_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_FloralSystem_RUNTIME_DLLS E:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
